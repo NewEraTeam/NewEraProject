03707352cb77501d7a3989d7112d5a37c593cebd
KU MUHAMMAD DANIEL BIN KU SAHRUL AFFANDI <kumuhammaddaniel@graduate.utm.my>
2024-12-10 02:16:08 +0800

    Booking page

--- NewEra/routes/web.php
+++ NewEra/routes/web.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\BadmintonController;
 use App\Http\Controllers\SwimmingController;
 use App\Http\Controllers\StadiumController;
 use App\Http\Controllers\GymController;
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\CustomerBookingController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
+>>>>>>> Stashed changes
 
 // Default route to load the login page
 Route::get('/', [UserDataController::class, 'showLogin'])->name('login.page');
@@ -61,3 +67,38 @@ Route::get('/facility/gym', function () {
 })->name('facility.gym');
 
 Route::get('/facility/gym', [GymController::class, 'index'])->name('gym');
+<<<<<<< Updated upstream
+=======
+
+// Customer booking history page
+Route::get('/customer-booking', function(){
+    return view('CustomerBookingModule.CustomerBooking'); // Corrected path for the customer-booking
+})->name('customer-booking');
+
+// About Us page
+Route::get('/about-us', function () {
+    return view('AboutUsModule.AboutUs'); // Corrected path for the About Us view
+})->name('about-us');
+
+//View Profile Page
+Route::get('/view-profile', function(){
+    return view('ViewProfileModule.ViewProfile');
+})->name('view-profile');
+
+Route::get('/booking/badminton', fn() => view('bookingBadminton'));
+Route::post('/booking/badminton', [BookingController::class, 'submitBookingBadminton']);
+Route::get('/booking/badminton', function () {
+    return view('BookingModule.bookingBadminton');
+})->name('bookingBadminton');
+
+Route::get('/booking/personal-details', fn() => view('BookingPersonalDetails'));
+Route::post('/booking/personal-details', [BookingController::class, 'storePersonalDetails']);
+Route::get('/booking/personal-details', function () {
+    return view('BookingModule.BookingPersonalDetails');
+})->name('bookingPersonalDetails');
+
+Route::get('/booking/payment', fn() => view('Payment'));
+Route::post('/booking/payment', [BookingController::class, 'storePaymentDetails']);
+
+Route::get('/booking/success', [BookingController::class, 'success']);
+>>>>>>> Stashed changes
