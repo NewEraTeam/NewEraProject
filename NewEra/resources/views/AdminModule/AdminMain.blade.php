641dc70468917bfb3f789fc7c1bc4d73f5e6cb8b
Navveen <navveennair188@gmail.com>
2024-12-11 00:41:00 +0800

    Update AdminMain.blade.php

--- NewEra/resources/views/AdminModule/AdminMain.blade.php
+++ NewEra/resources/views/AdminModule/AdminMain.blade.php
@@ -17,14 +17,7 @@
                 <img src="{{ asset('UTM-LOGO-FULL.png') }}" alt="UTM Logo" class="h-8 md:h-10" />
             </a>
 
-    <header class="relative mx-auto bg-center bg-cover h-96" style="background-image: url('https://shorturl.at/iSKRY');">
-        <div class="absolute inset-0 bg-black opacity-40"></div>
-            <div class="container relative flex flex-col items-center justify-center h-full mx-auto text-center text-white">
-            <h1 class="mb-4 text-4xl font-bold"> UTM Sports Management </h1>
-            <p class="mb-6 text-lg">Welcome to Admin  </p>
-        </div>
-    </header>
-
+    
             <!-- Profile Button -->
             <div class="relative inline-block text-left">
                 <button onclick="toggleAdminProfileMenu()" class="flex items-center space-x-2 bg-blue-100 text-blue-600 px-3 py-1.5 rounded-full font-semibold hover:bg-blue-200 focus:outline-none">
@@ -39,9 +32,19 @@
         </div>
     </nav>
 
+
+
     <!-- Dashboard Section -->
     <main class="container px-4 py-8 mx-auto">
         <h1 class="mb-6 text-2xl font-bold text-center">Admin Dashboard</h1>
+    <header class="relative mx-auto bg-center bg-cover h-96" style="background-image: url('https://shorturl.at/iSKRY');">
+        <div class="absolute inset-0 bg-black opacity-40"></div>
+            <div class="container relative flex flex-col items-center justify-center h-full mx-auto text-center text-white">
+            <h1 class="mb-4 text-4xl font-bold"> UTM Sports Management </h1>
+            <p class="mb-6 text-lg"> Welcome to Admin's Page </p>
+        </div>
+    </header>
+
 
         <!-- Admin Actions -->
         <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
